93b1212d3039961649d8e2d09f6aee4b45b2e7d9
lizhieffe <lizhieffe@gmail.com>
2017-08-21 14:51:15 -0700

    02

--- source/boot_section_simple.asm
+++ source/boot_section_simple.asm
@@ -1,8 +1,20 @@
+mov ah, 0x0e    ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+int 0x10
+mov al, 'O'
+int 0x10
+
 ; Infinite loop
 loop:
   jmp loop
 
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
+
 ; Magic number
 dw 0xaa55
